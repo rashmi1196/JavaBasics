8b1ce00debd48be5894bdd258b1db9fe525947ad
Rashmi Raj <Rashmi.Raj@jda.com>
2022-09-06 16:22:12 +0530

    first commit

---
+++ src/src/Demo.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Demo {
+
+
+
+    public static void main(String[] args) {
+        List al = new ArrayList<String>();
+        al.add("Demo");
+        al.add("Java");
+        al.add("Collection");
+
+        System.out.println(al);
+
+        System.out.println("Index of demo, after adding it again");
+        al.add("Demo");
+        al.add("java");
+        System.out.println(al);
+        System.out.println(al.lastIndexOf("Demo"));
+
+
+        System.out.println("Add Array at third index");
+        al.add(3,"Array");
+        System.out.println("===================list after adding value at index 3===============");
+        Iterator itr1 = al.iterator();
+        while(itr1.hasNext()){
+            System.out.printf(itr1.next()+" ");
+        }
+
+        System.out.println("\n============Cloning array list==============");
+        List arrayListClone =  (ArrayList<String>) ((ArrayList<?>) al).clone();
+        System.out.println(arrayListClone);
+
+        System.out.println("===========================Trimming the size====================");
+        ((ArrayList<?>) al).trimToSize();
+        System.out.println(al);
+
+        System.out.println("==============Converting to Array====================");
+        System.out.println(al.toArray());
+
+
+
+        System.out.println("clear arraylist");
+        al.clear();
+        System.out.println(al);
+
+
+    }
+}
